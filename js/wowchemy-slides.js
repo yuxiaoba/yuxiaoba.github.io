5e63add38b4e41f301ac9c7a745d6b1d3b64d83b
yuxiaoba <444723257@qq.com>
2022-02-02 11:12:28 +0800

    fix error

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/var/folders/_j/06cn9641395cmyn151spgp_00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20211231133358-6d4989f90f7a/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/_j/06cn9641395cmyn151spgp_00000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20220122152657-2ccc34471cfa/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
